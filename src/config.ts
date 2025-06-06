c97ca682dddda9e01c2465935f8cd09f1eb28c6c
kaksv <kakoozavian@gmail.com>
2025-06-06 09:47:31 +0300

    changes

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 import { http, createConfig } from 'wagmi'
-import { base, mainnet, optimism } from 'wagmi/chains'
+import { base, mainnet } from 'wagmi/chains'
 import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
 
 const projectId = import.meta.env.VITE_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
